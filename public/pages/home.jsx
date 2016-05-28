3131002b6f15e8430d4a2fb9de3aeef951fcc7e4
Cameron Chang <cameron@posse.com>
2016-05-28 22:02:20 +1000

    Updating project to make use of react
    - Also testing out webpack build

---
+++ public/pages/home.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import MainApp from '../javascripts';
+
+ReactDom.render(
+  <div>
+    <div className='mainContent'>Home page</div>
+  </div>,
+  document.getElementById('content')
+);
