e26fc32e4eb6a9e7e85b730b26f8c051353cc267
Cameron Chang <cameron@posse.com>
2016-05-31 16:02:03 +1000

    Added in store
    - getters now pull values from store

--- core/modules/main/getters.js
+++ core/modules/main/getters.js
@@ -1,29 +1,15 @@
 import { toImmutable } from 'nuclear-js';
 
 const subTopics = [
-  [],
-  () => {
-    var subtopics = [
-      { index: 1, title: "Triangles", completed: true },
-      { index: 2, title: "Angle Sum", completed: true },
-      { index: 3, title: "Similar Triangles", completed: true },
-      { index: 4, title: "Congruence", completed: false },
-      { index: 5, title: "Enlargements", completed: false },
-      { index: 6, title: "Parallel Lines", completed: false },
-      { index: 7, title: "Pythagoras Theorem", completed: false },
-      { index: 8, title: "Revision", completed: false },
-      { index: 9, title: "Topic Test", completed: false }
-    ];
+  ['mainStore', 'data', 'subtopics'],
+  (subtopics) => {
     return subtopics;
   },
 ];
 
 const topicTitle = [
-  [],
-  () => {
-    var topic = {
-      title: "Geometry"
-    };
+  ['mainStore', 'data', 'topic'],
+  (topic) => {
     return topic.title;
   },
 ];
